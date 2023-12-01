6a82988f13a78113392267d7167065dd19b3c83b
KMflutter <kmflutterteam@gmail.com>
2023-12-01 19:38:52 +0330

    Folder structure changed First Layouts and templates added slug dynamic routes first config set

---
+++ src/app/blog/[slug]/page.tsx
@@ -0,0 +1,6 @@
+export default async function BlogDetails({params}: { params: { slug: string } }) {
+    const {slug} = params;
+    return(
+        <h1 className='text-5xl flex justify-center items-center'>{slug}</h1>
+    )
+}
