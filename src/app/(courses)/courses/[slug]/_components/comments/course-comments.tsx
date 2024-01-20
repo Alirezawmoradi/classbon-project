a842e7a868b79f1de483fd351e5e06ab872ae6d5
Alirezaw <Alirezawmoradi@gmail.com>
2024-01-20 23:31:03 +0330

    infinite scroll feature added to course comments components

--- src/app/(courses)/courses/[slug]/_components/comments/course-comments.tsx
+++ src/app/(courses)/courses/[slug]/_components/comments/course-comments.tsx
@@ -3,26 +3,44 @@ import {useParams} from "next/navigation";
 import {useCourseComments} from "@/app/(courses)/courses/[slug]/_apis/get-comments";
 import {Comment} from "@/app/_components/comment";
 import {TextPlaceholder} from "@/app/_components/palceholders/text/text-placeholder";
+import {Fragment, useEffect} from "react";
+import {useInView} from "react-intersection-observer";
 
 const CourseComments = () => {
+    const {ref, inView} = useInView({})
     const {slug} = useParams();
-    const {data: comments,isLoading} = useCourseComments({
+    const {data: comments, error, isFetchingNextPage, fetchNextPage, hasNextPage, refetch} = useCourseComments({
         params: {
             slug: slug as string,
             page: 1
         }
     })
+    useEffect(() => {
+        if (inView && hasNextPage) {
+            fetchNextPage();
+        }
+    }, [inView, fetchNextPage, hasNextPage]);
     return (
         <>
             {
-                comments?.data.map(comment => (
-                        <Comment key={`comment-${comment.id}`} {...comment} variant="info"/>
-                    )
-                )
+                comments?.pages.map((currentPage) => (
+                    <Fragment key={`comment-page-${currentPage}`}>
+                        {
+                            currentPage.data.map((comment) => (
+                                <Comment key={`comment-${comment.id}`} {...comment} variant="info"/>
+                            ))
+                        }
+                    </Fragment>
+                ))
             }
             {
-                isLoading && <TextPlaceholder/>
+                (isFetchingNextPage || hasNextPage) && (
+                    <div ref={ref}>
+                        <TextPlaceholder/>
+                    </div>
+                )
             }
+
         </>
     )
 }
