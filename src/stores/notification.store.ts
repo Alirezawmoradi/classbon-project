3b8b2bf01a86936a922905b43bf51b83bf7ea55e
Alirezaw <Alirezawmoradi@gmail.com>
2024-01-31 18:31:08 +0330

    notificationStore created using Zustand state management

---
+++ src/stores/notification.store.ts
@@ -0,0 +1,29 @@
+import {Notification} from "@/types/notification.interface";
+import {create} from "zustand";
+import {generateID} from "@/utils/string";
+
+type NotificationState = {
+    notifications: Notification[];
+    showNotification: (notification: Omit<Notification, 'id'>) => void;
+    dismissNotification: (id: string) => void;
+}
+
+export const useNotificationStore = create<NotificationState>(
+    (set, get) => ({
+        notifications: [],
+        showNotification: (notification) => {
+            const id = generateID();
+            set(state => ({
+                notifications: [...state.notifications, {id: id, ...notification}]
+            }));
+            setTimeout(() => {
+                get().dismissNotification(id);
+            }, 5000);
+        },
+        dismissNotification: (id) => {
+            set(state => ({
+                notifications: state.notifications.filter(p => p.id !== id)
+            }));
+        }
+    })
+)
