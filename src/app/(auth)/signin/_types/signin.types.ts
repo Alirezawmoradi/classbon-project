ad29ef378a818a2acd5a82fc497b47b7ec76c08e
Alirezaw <Alirezawmoradi@gmail.com>
2024-06-20 18:48:56 +0330

    SigIn type refactored using zod

--- src/app/(auth)/signin/_types/signin.types.ts
+++ src/app/(auth)/signin/_types/signin.types.ts
@@ -1,3 +1,4 @@
-export interface SignIn {
-    mobile: string;
-}
+import {z} from "zod";
+import {SignInSchema} from "@/app/(auth)/signin/_types/signin.schema";
+
+export type SignIn= z.infer<typeof SignInSchema>
