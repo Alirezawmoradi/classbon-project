5d56b4ada55e0094d22228eccc99edfc0db3ad6a
Alirezaw <Alirezawmoradi@gmail.com>
2024-06-22 01:57:10 +0330

    zod validation added to signInAction for server action

--- src/actions/auth/index.ts
+++ src/actions/auth/index.ts
@@ -1,8 +1,17 @@
 'use server'
 
 import {redirect} from "next/navigation";
+import {signInSchema} from "@/app/(auth)/signin/_types/signin.schema";
 
 export async function signInAction(mobile: string) {
-    console.log(mobile);
-    redirect('/');
+
+    const validatedData = signInSchema.safeParse({
+        mobile
+    });
+    if (!validatedData.success) {
+        console.log('error');
+    } else {
+        console.log(mobile);
+        redirect('/');
+    }
 }
