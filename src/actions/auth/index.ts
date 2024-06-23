ddc79d91a82ca929cc322cc92a1b2c0070d5a5e6
Alirezaw <Alirezawmoradi@gmail.com>
2024-06-23 23:09:37 +0330

    validate user data in server action

--- src/actions/auth/index.ts
+++ src/actions/auth/index.ts
@@ -3,15 +3,24 @@
 import {redirect} from "next/navigation";
 import {signInSchema} from "@/app/(auth)/signin/_types/signin.schema";
 
-export async function signInAction(mobile: string) {
+export async function signInAction(formState: { message: string }, formData: FormData) {
+
+    const mobile = formData.get('mobile');
 
     const validatedData = signInSchema.safeParse({
         mobile
     });
     if (!validatedData.success) {
-        console.log('error');
+        return {
+            message: 'خطا در فرمت موبایل'
+        }
     } else {
-        console.log(mobile);
-        redirect('/');
+        try {
+            throw 'خطا در برقراری ارتباط با سرور'
+        } catch (error) {
+            return {
+                message: error as string
+            }
+        }
     }
 }
