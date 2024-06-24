958539ce0bbf9a0b0904b45de8a887ecc46105bf
Alirezaw <Alirezawmoradi@gmail.com>
2024-06-24 16:00:40 +0330

    server action wrapper created using operation result types

---
+++ src/actions/server-actions-wrapper.ts
@@ -0,0 +1,20 @@
+import { Problem } from "@/types/http-errors.interface";
+import { OperationResult } from "@/types/operation-result";
+
+export async function serverActionWrapper<T>(
+    action: () => Promise<T>
+): Promise<OperationResult<T>> {
+    try {
+        const response = await action();
+        return { isSuccess: true, response };
+    } catch (error: unknown) {
+        const err = error as Problem;
+        if (err) {
+            return {
+                isSuccess: false,
+                error: err,
+            };
+        }
+    }
+    throw new Error("خطای ناشناخته");
+}
