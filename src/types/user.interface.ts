0718b61142a815fc472b6aac71917c0ec647e0e3
Alirezaw <Alirezawmoradi@gmail.com>
2024-07-21 20:10:08 +0330

    user token interface created

--- src/types/user.interface.ts
+++ src/types/user.interface.ts
@@ -1,3 +1,11 @@
 export interface User {
     token: string;
 }
+
+export interface UserToken {
+    mobile?: string;
+    image?: number;
+    full_name?: string;
+    exp: number;
+    accessToken: string;
+}
