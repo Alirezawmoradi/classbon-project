46e4a51c7e2c76da3c1a488613314b515a14ac62
Alirezaw <Alirezawmoradi@gmail.com>
2024-10-07 23:32:19 +0330

    session test for show login information

--- src/app/_components/header/header.tsx
+++ src/app/_components/header/header.tsx
@@ -1,6 +1,11 @@
 import Image from "next/image";
-import { TopNavigation } from "./top-navigation";
-export const Header: React.FC = () => {
+import {TopNavigation} from "./top-navigation";
+import {auth} from "@/auth";
+
+
+export const Header: React.FC = async () => {
+    const session = await auth()
+
     return (
         <header className="border-b dark:border-base-content dark:border-opacity-5">
             <div className="container flex items-center justify-between">
@@ -10,8 +15,8 @@ export const Header: React.FC = () => {
                     height={36}
                     alt="کلاسبن"
                 />
-                <TopNavigation />
-                <div className="mr-auto">User Authentication</div>
+                <TopNavigation/>
+                <div className="mr-auto">{session?.user.mobile}</div>
             </div>
         </header>
     );
