0133869f115275a255d2c1f95b3b8437ff98aca7
Alirezaw <Alirezawmoradi@gmail.com>
2024-10-08 00:10:10 +0330

    auth provider added to application

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import {Footer} from "@/app/_components/footer";
 import QueryProvider from "@/providers/react-query-provider";
 import NextTopLoader from "nextjs-toploader";
 import {Notifications} from "@/app/_components/notification/notifications";
+import {AuthProvider} from "@/providers/auth-provider";
 
 const figtree = Figtree({
     display: 'swap',
@@ -58,13 +59,15 @@ export default function RootLayout({
         <body className='min-h-screen grid grid-rows-[80px_1fr_auto] dark:bg-base-100 dark:text-base-content'>
         <NextTopLoader showSpinner={false} color='var(--color-primary)'/>
         <Notifications/>
-        <QueryProvider>
-            <Header/>
-            <main>
-                {children}
-            </main>
-            <Footer/>
-        </QueryProvider>
+        <AuthProvider>
+            <QueryProvider>
+                <Header/>
+                <main>
+                    {children}
+                </main>
+                <Footer/>
+            </QueryProvider>
+        </AuthProvider>
         </body>
         </html>
     )
