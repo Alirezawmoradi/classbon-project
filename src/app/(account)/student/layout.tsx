9b4439419b0de3d6fa946ceccf26b03c3274399c
Alirezaw <Alirezawmoradi@gmail.com>
2024-10-08 23:51:32 +0330

    private route added

--- src/app/(account)/student/layout.tsx
+++ src/app/(account)/student/layout.tsx
@@ -1,12 +1,19 @@
-import {ReactNode} from "react";
+import { auth } from "@/auth"
+import { redirect } from "next/navigation";
 
-export default function StudentLayout({children}: { children: ReactNode }) {
+export default  async function StudentLayout({
+                                                 children,
+                                             }: {
+    children: React.ReactNode
+}) {
+    const session = await auth();
+    if (!session || !session.user) {
+        redirect("/signin");
+    }
     return (
         <>
-            <aside className='bg-gray-300 w-80 flex justify-center items-center'>
-
-            </aside>
-            <main>
+            <aside className="bg-gray-300 w-80 flex justify-center items-center self-stretch text-3xl">STUDENT SIDEBAR</aside>
+            <main className="flex justify-center items-center flex-1 text-5xl">
                 {children}
             </main>
         </>
