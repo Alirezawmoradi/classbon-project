93cd14e823a3bfbe64251dfe5a17e19a2e57f573
Alirezaw <Alirezawmoradi@gmail.com>
2024-10-13 18:22:01 +0330

    refactored

--- src/app/(account)/student/layout.tsx
+++ src/app/(account)/student/layout.tsx
@@ -1,18 +1,12 @@
-import { auth } from "@/auth"
-import { redirect } from "next/navigation";
-
-export default  async function StudentLayout({
-                                                 children,
-                                             }: {
+export default async function StudentLayout({
+                                                children,
+                                            }: {
     children: React.ReactNode
 }) {
-    const session = await auth();
-    if (!session || !session.user) {
-        redirect("/signin");
-    }
     return (
         <>
-            <aside className="bg-gray-300 w-80 flex justify-center items-center self-stretch text-3xl">STUDENT SIDEBAR</aside>
+            <aside className="bg-gray-300 w-80 flex justify-center items-center self-stretch text-3xl">STUDENT SIDEBAR
+            </aside>
             <main className="flex justify-center items-center flex-1 text-5xl">
                 {children}
             </main>
