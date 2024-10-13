06db53f85d656163f9077b5ffe6a530250b64843
Alirezaw <Alirezawmoradi@gmail.com>
2024-10-13 18:21:40 +0330

    middleware set for authorized user

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -1,9 +1,26 @@
 import {NextAuthConfig} from "next-auth";
+import {response} from "express";
 
 export const authConfig = {
     pages: {
         signIn: '/signin'
     },
-    callbacks: {},
+    callbacks: {
+        async authorized({auth, request}) {
+            const {nextUrl} = request;
+
+            const isAuthenticated = !!auth?.user;
+
+            const authRoutes = ['/signin', '/verify'];
+            const isAuthRoutes = authRoutes.includes(nextUrl.pathname);
+
+            if (isAuthRoutes && isAuthenticated) {
+                return Response.redirect(new URL('/student/courses', nextUrl));
+            }
+
+            return true;
+
+        }
+    },
     providers: []
 } satisfies NextAuthConfig
